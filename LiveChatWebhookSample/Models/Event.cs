642ac371756aced66e4a6343e28841a368132c32
phoenixinobi <phoenixinobi@gmail.com>
2017-05-19 11:18:51 +0800

    Updated Event.cs to reflect json property convention

--- LiveChatWebhookSample/Models/Event.cs
+++ LiveChatWebhookSample/Models/Event.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace LiveChatWebhookSample.Models
 {
+    using Newtonsoft.Json;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -8,16 +9,22 @@ namespace LiveChatWebhookSample.Models
 
     public class Event
     {
+        [JsonProperty("author")]
         public Author Author { get; set; }
 
+        [JsonProperty("date")]
         public string Date { get; set; }
 
+        [JsonProperty("is_private")]
         public bool IsPrivate { get; set; }
 
+        [JsonProperty("message")]
         public string Message { get; set; }
 
+        [JsonProperty("source")]
         public Source Source { get; set; }
 
+        [JsonProperty("type")]
         public string Type { get; set; }
     }
 }
